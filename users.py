ffd2c2511e7631b21f1dc5223163b8868c54caf7
Djams0 <162589075+Djams0@users.noreply.github.com>
2024-11-27 16:28:56 +0100

    Update users.py

--- users.py
+++ users.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
 class users(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     date = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(blank=True, null=True)
 
+
+USERNAME_FIELD = 'email'
+REQUIRED_FIELDS = []
