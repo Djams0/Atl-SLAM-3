577eb2149f42ffebce53d4513871b04d04fb5833
Mansour Djamil NDIAYE <162589075+Djams0@users.noreply.github.com>
2025-04-09 16:03:12 +0200

    Delete Musify directory

--- Musify/Musify/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .favorite import Favorite
-from .historycal import historical
-from .Playlist import playlist
-from .Playlist_user import PlaylistUser
-
--- Musify/Musify/models/playlist.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-class playlist(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
-    
