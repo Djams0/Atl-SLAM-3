a8be80deb1e49e1a6df19405f207fcc6dc657aad
Mansour Djamil NDIAYE <162589075+Djams0@users.noreply.github.com>
2025-04-09 11:24:41 +0200

    Delete historycal.py

--- historycal.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-class historycal(models.Model):
-    user = models.ForeignKey("Musify.users", verbose_name=_("musify_users"), on_delete=models.CASCADE)
-    id_song_deezer = models.BigIntegerField()
-    date = models.DateTimeField(auto_now_add=True)
